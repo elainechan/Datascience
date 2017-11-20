2d8c1386ffa296e40fddab49f5ff89bf0d29955f
Elaine Chan <elaine.chan@outlook.com>
2017-11-19 22:46:51 -0500

    toy dash app, design notes

---
+++ dash/app.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# reference: "https://plot.ly/dash/getting-started"
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.H1(children="Robert and Jane's Arranged Marriage"),
+
+    html.Div(children='''
+        A Happy Ending?
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                { 
+                    'x': [0, 10, 20, 30],
+                    'y': [0, 1, 2, 4],
+                    'type': 'scatter',
+                    'mode': 'lines',
+                    'line': {
+                        'width': 8, # set line width
+                        'color': '#edf8b1' # set line color
+                    },
+                    'name': 'Jane'
+                },
+                {
+                    'x': [0, 10, 20, 30],
+                    'y': [8, 9, 5, 4],
+                    'type': 'scatter',
+                    'mode': 'lines',
+                    'line': {
+                        'width': 8,
+                        'color': '#2c7fb8'
+                    },
+                    'name': 'Robert'            
+                }
+            ],
+            'layout': {
+                'title': 'Happiness Levels Over the Years',
+                'xaxis': {
+                    'title': 'Years Elapsed Since Wedding',
+                    'showgrid': False
+                },
+                'yaxis': {
+                    'title': 'Happiness Levels',
+                    'showgrid': False
+                }
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ dash/dash.md
@@ -0,0 +1,13 @@
+# Design Tips
+
+* Tighten margins
+```python
+'layout': {
+    'margin': {
+        'l': 50, # left margin. Use 20 if you don't have a yaxis title
+        'r': 10, # right margin. This can always be small unless you have annotations
+        't': 50, # top margin. Use 10 if you don't have a title
+        'b': 50, # bottom margin. Use 30 if you don't have a xaxis title
+    }
+}
+```
